9ce654db57a99d997866b555176c0804b0840d14
Jared S <jaredjames2020@gmail.com>
2019-10-16 21:17:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,12 +14,12 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       
-      projects: kickstarter.css("li.project.grid_4"),
-      title: project.css("h2.bbcard_name strong a").text,
-      image link: project.css("div.project-thumbnail a img").attribute("src").value,
-      description: project.css("p.bbcard_blurb").text,
-      location: project.css("span.location-name").text,
-      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+      :project => kickstarter.css("li.project.grid_4"),
+      :title => project.css("h2.bbcard_name strong a").text,
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
       }
   end 
   projects
